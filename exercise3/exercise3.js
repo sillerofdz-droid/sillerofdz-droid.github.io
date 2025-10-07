88d1d52b6158908cea8c73e9856d288ea027e36a
David <sillerofdz@gmail.com>
2025-10-07 17:12:43 +0200

    nuevo cambio

---
+++ exercise3/exercise3.js
@@ -0,0 +1,19 @@
+function turnOn () {
+
+    document.getElementById('myImage').src='images/pic_bulbon.gif';
+    document.getElementById('letrero').innerHTML="ITS TURNED ON! :D"
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+}
+
+function turnOff () {
+
+    document.getElementById('myImage').src='images/pic_bulboff.gif';
+    document.getElementById('letrero').innerHTML="ITS TURNED OF AGAIN :_(";
+    document.querySelector('.plon').innerHTML="ITS TURNED OF AGAIN :_(";
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "yellow";
+}
+
+
+
